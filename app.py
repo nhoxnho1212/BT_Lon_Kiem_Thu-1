a1031e7e13eb16786f68042b8df72bc1e49fca01
Tu Minh <duongtrantuminh14@gmail.com>
2019-11-13 15:43:17 +0700

    complete diem_danh api

--- app.py
+++ app.py
@@ -18,284 +18,31 @@ app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 mongo = MongoClient('localhost', 27017)
 db_teacher = mongo.db.teachers
 db_classes = mongo.db.classes
-db_checkin = mongo.db.checkin
 db_students = mongo.db.students
-# db_classes.delete_many({})
-# classes = [{
-#     "id":"ITECT001",
-#     "name":"CO SO DU LIEU",
-#     "student": [
+
+# db_students.delete_many({})
+# minh_face = face_encodings(load_image_file('tuminh.jpg'), num_jitters = 100)[0]
+# tung_face = face_encodings(load_image_file('tung.jpg'),num_jitters = 100)[0]
+# thien_face = face_encodings(load_image_file('thien.jpg'),num_jitters = 100)[0]
+# students = [
 #     {
-#         "id":"1751010082",
-#         "birthday":"14-06-1999",
-#         "name":"DUONG TRAN TU MINH"
+#         "ho_ten": "Duong Tran Tu Minh",
+#         "id": "1751010082",
+#         "encoded_face" : list(minh_face)
 #     },
 #     {
-#         "id":"1751010180",
-#         "birthday":"14-06-1999",
-#         "name":"DO NGUYEN THANH TUNG"
-#     },
-# {
-#         "id":"1751012068",
-#         "birthday":"14-06-1999",
-#         "name":"NGUYEN TRAN NHAT THIEN"
-#     }],
-#     "DateCheckin": [
-#         {
-#             "date": '20-10-2019',
-#             "student":[
-#                         {
-#                             "studentID":"1751010082",
-#                             "status": 0
-#                         },
-#                         {
-#                             "studentID":"1751010180",
-#                             "status": 0
-#                         },
-#                         {
-#                             "studentID":"1751012068",
-#                             "status": 0
-#                         },
-# ]
-#         },
-# {
-#             "date": '27-10-2019',
-#             "student":[
-#                         {
-#                             "studentID":"1751010082",
-#                             "status": 0
-#                         },
-#                         {
-#                             "studentID":"1751010180",
-#                             "status": 0
-#                         },
-#                         {
-#                             "studentID":"1751012068",
-#                             "status": 0
-#                         },
-# ]
-#         },
-# {
-#             "date": '3-11-2019',
-#             "student":[
-#                         {
-#                             "studentID":"1751010082",
-#                             "status": 0
-#                         },
-#                         {
-#                             "studentID":"1751010180",
-#                             "status": 0
-#                         },
-#                         {
-#                             "studentID":"1751012068",
-#                             "status": 0
-#                         },
-# ]
-#         },
-# {
-#             "date": '10-11-2019',
-#             "student":[
-#                         {
-#                             "studentID":"1751010082",
-#                             "status": 0
-#                         },
-#                         {
-#                             "studentID":"1751010180",
-#                             "status": 0
-#                         },
-#                         {
-#                             "studentID":"1751012068",
-#                             "status": 0
-#                         },
-# ]
-#         }
-#     ]
-#
-# },{
-#     "id":"ITECT002",
-#     "name":"XU LY ANH",
-#     "student": [
-#     {
-#         "id":"1751010082",
-#          "birthday":"14-06-1999",
-#         "name":"NGUYEN THI TRIEU"
+#         "ho_ten": "Do Nguyen Thanh Tung",
+#         "id": "1751010180",
+#         "encoded_face" : list(tung_face)
 #     },
 #     {
-#         "id":"1751010180",
-#         "birthday":"14-06-1999",
-#         "name":"DO NGUYEN THANH TUNG"
-#     },
-# {
-#         "id":"1751012068",
-#         "birthday":"14-06-1999",
-#         "name":"NGUYEN TRAN NHAT THIEN"
-#     }],
-#     "DateCheckin": [
-#         {
-#             "date": '20-10-2019',
-#             "student":[
-#                         {
-#                             "studentID":"1751010082",
-#                             "status": 0
-#                         },
-#                         {
-#                             "studentID":"1751010180",
-#                             "status": 0
-#                         },
-#                         {
-#                             "studentID":"1751012068",
-#                             "status": 0
-#                         },
+#         "ho_ten": "Nguyen Tran Nhat Thien",
+#         "id": "1751012068",
+#         "encoded_face" : list(thien_face)
+#     }
 # ]
-#         },
-# {
-#             "date": '27-10-2019',
-#             "student":[
-#                         {
-#                             "studentID":"1751010082",
-#                             "status": 0
-#                         },
-#                         {
-#                             "studentID":"1751010180",
-#                             "status": 0
-#                         },
-#                         {
-#                             "studentID":"1751012068",
-#                             "status": 0
-#                         },
-# ]
-#         },
-# {
-#             "date": '3-11-2019',
-#             "student":[
-#                         {
-#                             "studentID":"1751010082",
-#                             "status": 0
-#                         },
-#                         {
-#                             "studentID":"1751010180",
-#                             "status": 0
-#                         },
-#                         {
-#                             "studentID":"1751012068",
-#                             "status": 0
-#                         },
-# ]
-#         },
-# {
-#             "date": '10-11-2019',
-#             "student":[
-#                         {
-#                             "studentID":"1751010082",
-#                             "status": 0
-#                         },
-#                         {
-#                             "studentID":"1751010180",
-#                             "status": 0
-#                         },
-#                         {
-#                             "studentID":"1751012068",
-#                             "status": 0
-#                         },
-# ]
-#         }
-#     ]
-#
-# },
-# {
-#     "id":"ITECT003",
-#     "name":"KIEM THU",
-#     "student": [
-#     {
-#         "id":"1751010082",
-#         "birthday":"14-06-1999",
-#         "name":"DUONG TRAN TU MINH"
-#     },
-#     {
-#         "id":"1751010180",
-#         "birthday":"14-06-1999",
-#         "name":"NGUYEN HOANG HUY"
-#     },
-# {
-#         "id":"1751012068",
-#         "birthday":"14-06-1999",
-#         "name":"NGUYEN TRAN NHAT THIEN"
-#     }],
-#     "DateCheckin": [
-#         {
-#             "date": '20-10-2019',
-#             "student":[
-#                         {
-#                             "studentID":"1751010082",
-#                             "status": 0
-#                         },
-#                         {
-#                             "studentID":"1751010180",
-#                             "status": 0
-#                         },
-#                         {
-#                             "studentID":"1751012068",
-#                             "status": 0
-#                         },
-# ]
-#         },
-# {
-#             "date": '27-10-2019',
-#             "student":[
-#                         {
-#                             "studentID":"1751010082",
-#                             "status": 0
-#                         },
-#                         {
-#                             "studentID":"1751010180",
-#                             "status": 0
-#                         },
-#                         {
-#                             "studentID":"1751012068",
-#                             "status": 0
-#                         },
-# ]
-#         },
-# {
-#             "date": '3-11-2019',
-#             "student":[
-#                         {
-#                             "studentID":"1751010082",
-#                             "status": 0
-#                         },
-#                         {
-#                             "studentID":"1751010180",
-#                             "status": 0
-#                         },
-#                         {
-#                             "studentID":"1751012068",
-#                             "status": 0
-#                         },
-# ]
-#         },
-# {
-#             "date": '10-11-2019',
-#             "student":[
-#                         {
-#                             "studentID":"1751010082",
-#                             "status": 0
-#                         },
-#                         {
-#                             "studentID":"1751010180",
-#                             "status": 0
-#                         },
-#                         {
-#                             "studentID":"1751012068",
-#                             "status": 0
-#                         },
-# ]
-#         }
-#     ]
-#
-# }
-# ]
-# db_classes.insert_many(classes, ordered= False)
-# len()
+# db_students.insert_many(students, ordered= False)
+# # len()
 @app.route('/', methods=['GET'])
 def hello_word():
     return dumps({"status":'Welcome to my app'})
@@ -356,99 +103,114 @@ def get_a_student():
         return dumps({'status': int(0),
         'data': int(0)})
 
-@app.route('/add_sinhvien', methods=['POST'])
-def add_sinh_vien():
-    try:
-        data = request.form.to_dict()
-        id = data["id"]
-        name = data["name"]
-        birthday = data["birthday"]
-        classID = data["classID"].split(',')
-        if 'file' not in request.files:
-            flash('No file part')
-            return redirect(request.url)
-        file = request.files['file']
-        if file.filename == '':
-            flash('No selected file')
-            return redirect(request.url)
-        if file and allowed_file(file.filename):
-            filename = secure_filename(file.filename)
-            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename.split('.')[-2]+'_stored.jpeg'))
-            image = load_image_file(os.path.join(app.config['UPLOAD_FOLDER'], filename.split('.')[-2]+'_stored.jpeg'))
-            encoded_face = face_encodings(image, num_jitters=1)[0]
-        query = db_classes.find_one({"id": id})
-        if query:
-                return dumps({'status': 'sinh vien da co trong he thong'})
-        else:
-            if id and name and birthday:
-                new_sinhvien = {
-                    "id": id,
-                    "name": name,
-                    "birthday": birthday,
-                    "classID": classID,
-                    "khuon_mat_ma_hoa":  list(encoded_face)}
-                db_students.insert_one(new_sinhvien)
-            return dumps({'status': bool(True)})
-    except Exception as e:
-        print(e)
-        return dumps({'error': str(e)})
-
-@app.route("/get_all_sinhvien", methods = ['GET'])
-def get_all_sinhvien():
-    try:
-        sinh_vien = db_students.find()
-        a = list(sinh_vien)
-        print(a)
-        return dumps(sinh_vien)
-    except Exception as e:
-        return dumps({'error' : str(e)})
-@app.route("/delete_all", methods = ['DELETE'])
-def delete_all():
-    try:
-        db_students.delete_many({})
-        return dumps({"status": bool(True)})
-    except Exception as e:
-        return dumps({'error': str(e)})
-
-@app.route("/delete_sinhvien", methods = ['DELETE'])
-def delete_sinhvien():
-    try:
-        data = request.form.to_dict()
-        mssv = data["mssv"]
-        query = {"mssv": mssv}
-        a = mongo.db.sinhvien.delete_one(query)
-        if a.deleted_count == 0:
-            return dumps({"status": "sinh vien khong co trong he thong"})
-        return dumps({"status":bool(True)})
-    except Exception as e:
-        return dumps({'error' : str(e)})
+# @app.route('/add_sinhvien', methods=['POST'])
+# def add_sinh_vien():
+#     try:
+#         data = request.form.to_dict()
+#         id = data["id"]
+#         name = data["name"]
+#         birthday = data["birthday"]
+#         classID = data["classID"].split(',')
+#         if 'file' not in request.files:
+#             flash('No file part')
+#             return redirect(request.url)
+#         file = request.files['file']
+#         if file.filename == '':
+#             flash('No selected file')
+#             return redirect(request.url)
+#         if file and allowed_file(file.filename):
+#             filename = secure_filename(file.filename)
+#             file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename.split('.')[-2]+'_stored.jpeg'))
+#             image = load_image_file(os.path.join(app.config['UPLOAD_FOLDER'], filename.split('.')[-2]+'_stored.jpeg'))
+#             encoded_face = face_encodings(image, num_jitters=1)[0]
+#         query = db_classes.find_one({"id": id})
+#         if query:
+#                 return dumps({'status': 'sinh vien da co trong he thong'})
+#         else:
+#             if id and name and birthday:
+#                 new_sinhvien = {
+#                     "id": id,
+#                     "name": name,
+#                     "birthday": birthday,
+#                     "classID": classID,
+#                     "khuon_mat_ma_hoa":  list(encoded_face)}
+#                 db_students.insert_one(new_sinhvien)
+#             return dumps({'status': bool(True)})
+#     except Exception as e:
+#         print(e)
+#         return dumps({'error': str(e)})
+#
+# @app.route("/get_all_sinhvien", methods = ['GET'])
+# def get_all_sinhvien():
+#     try:
+#         sinh_vien = db_students.find()
+#         a = list(sinh_vien)
+#         print(a)
+#         return dumps(sinh_vien)
+#     except Exception as e:
+#         return dumps({'error' : str(e)})
+# @app.route("/delete_all", methods = ['DELETE'])
+# def delete_all():
+#     try:
+#         db_students.delete_many({})
+#         return dumps({"status": bool(True)})
+#     except Exception as e:
+#         return dumps({'error': str(e)})
+#
+# @app.route("/delete_sinhvien", methods = ['DELETE'])
+# def delete_sinhvien():
+#     try:
+#         data = request.form.to_dict()
+#         mssv = data["mssv"]
+#         query = {"mssv": mssv}
+#         a = mongo.db.sinhvien.delete_one(query)
+#         if a.deleted_count == 0:
+#             return dumps({"status": "sinh vien khong co trong he thong"})
+#         return dumps({"status":bool(True)})
+#     except Exception as e:
+#         return dumps({'error' : str(e)})
 
 def allowed_file(filename):
     return '.' in filename and \
            filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
-@app.route("/diem_danh", methods=["GET"])
+@app.route("/diem_danh", methods=["POST"])
 def diem_danh():
     try:
-        if 'file' not in request.files:
+        data = request.form.to_dict()
+        classId = data["classId"]
+        studentId = data["studentId"]
+        date = data['date']
+        if 'image' not in request.files:
             flash('No file part')
             return redirect(request.url)
-        file = request.files['file']
+        file = request.files['image']
         if file.filename == '':
             flash('No selected file')
             return redirect(request.url)
-        data = request.form.to_dict()
-        mssv = data["mssv"]
         if file and allowed_file(file.filename):
             filename = secure_filename(file.filename)
             file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename.split('.')[-2]+'_upload.jpg'))
-            query = {"mssv": mssv}
-            sinhvien = mongo.db.sinhvien.find_one(query)
-            input_face = load_image_file(os.path.join(app.config['UPLOAD_FOLDER'], filename.split('.')[-2]+'_upload.jpg'))
-            stored_face = np.asarray(sinhvien['khuon_mat_ma_hoa'])
-            diemdanh = dd(input_face,stored_face)
-            return dumps({"status": bool(diemdanh[0])})
+            query0 = {"id": classId}
+            class_0 = db_classes.find_one(query0)
+            for j,days in enumerate(class_0['DateCheckin']):
+                if days['date'] == date:
+                    for i,student in enumerate(days["student"]):
+                        if student['studentID'] == studentId:
+                            input_face = load_image_file(
+                                os.path.join(app.config['UPLOAD_FOLDER'], filename.split('.')[-2] + '_upload.jpg'))
+                            query = {"id": studentId}
+                            sinhvien = db_students.find_one(query)
+                            stored_face = np.asarray(sinhvien['encoded_face'])
+                            diemdanh = dd(input_face, stored_face)
+                            query_status = {
+                                "id": classId,
+                            }
+                            newvalues = {"$set": {"DateCheckin."+str(j)+".student."+str(i)+".status": 1}}
+                            db_classes.update_one(query_status, newvalues)
+                            return dumps({"status": db_classes.find_one({"id":classId})})
     except Exception as e:
-        return dumps({"status": str(e)})
+        print(e)
+        return dumps({"status": int(0)})
 if __name__ == '__main__':
         app.run(host='0.0.0.0',debug = True)
